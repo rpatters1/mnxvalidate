3efe843bbeea1896dc1364faacdb8bd719394a97
rpatters1 <rpatters1@users.noreply.github.com>
2025-04-11 10:12:37 -0500

    2.0 finishing touches (#11)
    * update for building 2.0
    
    * fix winprod.bat

---
+++ scripts/macprod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Change to the parent directory of the script
+cd "$(dirname "$0")/.."
+
+BUILD_DIR="build"
+rm -rf "$BUILD_DIR"
+
+# Configure the project with Ninja for Release
+echo "Configuring the project with Ninja..."
+cmake -G Ninja -S . -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=Release
+
+# Build the project
+echo "Building the project..."
+cmake --build "$BUILD_DIR"
+
+echo "Build completed successfully."
